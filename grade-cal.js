5fb16c889dba8991ef0e05fd34b02cd6d69e84f4
Laweens Francois <lfrancois@brooklynsteamcenter.org>
2023-04-17 14:53:03 -0400

    new stuff

---
+++ grade-cal.js
@@ -0,0 +1,6 @@
+let grade="77"
+if (grade >= 90) {console.log("your grade is currently an A")} 
+if (grade < 80 && grade > 89)  {console.log("your grade is currently a B")} else
+if (grade <79 && grade > 70) {console.log("your grade is currently a C")} else
+if (grade <69 && grade > 60) {console.log("your grade is currently a D")} 
+else {console.log("your grade is currently a F")}
---
+++ hello.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+let myName="Laweens"
+let myAge="16"
+console.log("Hello my name is " + myName + ". My age is " + myAge) 
+if (myAge >= 13) {
+    console.log("you are a teenager")
+} else { console.log("you are a child")}
+
+let myList =[ 12,7,4]
+for( let item of myList) {
+    console.log(item*12)
+}
